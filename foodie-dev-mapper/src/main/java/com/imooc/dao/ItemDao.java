49d0a2f00cb6bf66b449a952e8bd89a87b59f6f3
沈荣 <736644385@qq.com>
2022-05-25 23:42:36 +0800

    feat(首页): 查询商品详情dao实现

--- foodie-dev-mapper/src/main/java/com/imooc/dao/ItemDao.java
+++ foodie-dev-mapper/src/main/java/com/imooc/dao/ItemDao.java
@@ -1,5 +1,12 @@
 package com.imooc.dao;
 
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+
+import java.util.List;
+
 /**
  * 说明:
  *
@@ -7,4 +14,32 @@ package com.imooc.dao;
  * @date 2022/5/25 0:13
  */
 public interface ItemDao {
+
+    /**
+     * 根据id查询商品
+     * @param itemId
+     * @return
+     */
+    Items getById(String itemId);
+
+    /**
+     * 查询商品下的图片
+     * @param itemId
+     * @return
+     */
+    List<ItemsImg> listItemImages(String itemId);
+
+    /**
+     * 查询商品下规格
+     * @param itemId
+     * @return
+     */
+    List<ItemsSpec> listItemSpecs(String itemId);
+
+    /**
+     * 查询商品参数
+     * @param itemId
+     * @return
+     */
+    ItemsParam getItemParam(String itemId);
 }
--- foodie-dev-mapper/src/main/java/com/imooc/dao/impl/ItemDaoImpl.java
+++ foodie-dev-mapper/src/main/java/com/imooc/dao/impl/ItemDaoImpl.java
@@ -5,9 +5,15 @@ import com.imooc.mapper.ItemsImgMapper;
 import com.imooc.mapper.ItemsMapper;
 import com.imooc.mapper.ItemsParamMapper;
 import com.imooc.mapper.ItemsSpecMapper;
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
 import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 说明:
@@ -25,4 +31,33 @@ public class ItemDaoImpl implements ItemDao {
     private ItemsSpecMapper itemsSpecMapper;
     @Resource
     private ItemsParamMapper itemsParamMapper;
+
+    @Override
+    public Items getById(String itemId) {
+        return itemsMapper.selectByPrimaryKey(itemId);
+    }
+
+    @Override
+    public List<ItemsImg> listItemImages(String itemId) {
+        Example example = new Example(ItemsImg.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+        return itemsImgMapper.selectByExample(example);
+    }
+
+    @Override
+    public List<ItemsSpec> listItemSpecs(String itemId) {
+        Example example = new Example(ItemsSpec.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+        return itemsSpecMapper.selectByExample(example);
+    }
+
+    @Override
+    public ItemsParam getItemParam(String itemId) {
+        Example example = new Example(ItemsParam.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+        return itemsParamMapper.selectOneByExample(example);
+    }
 }

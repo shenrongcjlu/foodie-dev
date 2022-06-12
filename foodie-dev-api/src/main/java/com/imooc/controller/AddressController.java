e31489af830754a2919481eaf5cbfa941f221cb2
沈荣 <736644385@qq.com>
2022-06-12 19:50:42 +0800

    feat(购物车): 增加地址controller

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/AddressController.java
@@ -0,0 +1,20 @@
+package com.imooc.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 说明:
+ *
+ * @author 沈荣
+ * @date 2022/6/12 19:47
+ */
+@Api(tags = "收货地址管理")
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+
+
+}

068c00f3be0df038372d2167cf7cffab6d8f8be8
沈荣 <736644385@qq.com>
2022-11-16 00:02:55 +0800

    feat(用户中心): 增加用户头像上传接口

--- foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -1,5 +1,6 @@
 package com.imooc.controller.center;
 
+import com.imooc.LoginContext;
 import com.imooc.ResultDTO;
 import com.imooc.center.dto.request.UserUpdateRequestDTO;
 import com.imooc.portal.dto.UserDTO;
@@ -8,11 +9,16 @@ import com.imooc.utils.CookieUtils;
 import com.imooc.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 
 /**
  * 说明:
@@ -25,6 +31,11 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/userInfo")
 public class CenterUserController {
 
+    private static final String IMAGE_USER_FACE_LOCATION = File.separator + "workspace"
+            + File.separator + "images"
+            + File.separator + "foodie"
+            + File.separator + "faces";
+
     @Resource
     private CenterUserService centerUserService;
 
@@ -34,6 +45,13 @@ public class CenterUserController {
         UserDTO user = centerUserService.updateUserInfo(param);
         CookieUtils.setCookie(request, response, "user", JsonUtils.objectToJson(user), true);
         return ResultDTO.success(user);
+    }
+
+    @ApiOperation("上传用户头像")
+    @PostMapping("/uploadFace")
+    public ResultDTO<Void> uploadFace(MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
+        String faceFilePath = IMAGE_USER_FACE_LOCATION + File.separator + LoginContext.getUserId();
+        return ResultDTO.success();
     }
 
 }

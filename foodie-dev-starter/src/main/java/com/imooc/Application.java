c0ee7af6cba24271447e7fdd69f20f46613cf4dc
沈荣 <736644385@qq.com>
2023-03-30 21:16:21 +0800

    feat(pom): 整合spring session

--- foodie-dev-starter/src/main/java/com/imooc/Application.java
+++ foodie-dev-starter/src/main/java/com/imooc/Application.java
@@ -2,9 +2,11 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 说明
@@ -12,10 +14,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author 沈荣
  * @date 2022/5/21 18:23
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableScheduling
 @ComponentScan(basePackages = {"org.n3r.idworker", "com.imooc"})
 @PropertySource("classpath:file-upload-dev.properties")
+@EnableRedisHttpSession
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

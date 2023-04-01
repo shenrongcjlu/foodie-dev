d6ce31add0480795d2d11af260a71a4f74514500
沈荣 <736644385@qq.com>
2023-04-01 15:33:29 +0800

    feat(sso): 生成用户票据

--- foodie-sso/src/main/java/com/imooc/controller/SSOController.java
+++ foodie-sso/src/main/java/com/imooc/controller/SSOController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
@@ -59,12 +60,28 @@ public class SSOController {
             return "login";
         }
 
+        // 生成用户token
         String uniqueToken = UUID.randomUUID().toString().trim();
         userDTO.setUserUniqueToken(uniqueToken);
         redisOperator.set(Constants.USER_TOKEN + ":" + userDTO.getId(), JsonUtils.objectToJson(userDTO));
 
+        // 生成用户ticket
+        String userTicket = UUID.randomUUID().toString().trim();
+        redisOperator.set(Constants.USER_TICKET + ":" + userTicket, userDTO.getId());
+        addCookie(response, userTicket);
+
+        // 生成临时ticket
+        String userTmpTicket = UUID.randomUUID().toString().trim();
+        redisOperator.set(Constants.USER_TMP_TICKET + ":" + userTmpTicket, MD5Utils.getMD5Str(userTicket), 600);
+
         return "login";
     }
 
-
+    private void addCookie(HttpServletResponse response, String userTicket) {
+        // ticket放到cookie中
+        Cookie cookie = new Cookie(Constants.COOKIE_USER_TICKET, userTicket);
+        cookie.setDomain("sso.com");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
 }
